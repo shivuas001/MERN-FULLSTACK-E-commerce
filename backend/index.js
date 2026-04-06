83c2c7067beca722351417d4cd9de3e421c78ae7
A SHIVA KUMAR <shivuas605@gmail.com>
2026-04-06 19:39:43 +0530

    created upload endpoint for images using multer

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const app = express();
 const mongoose = require("mongoose");
 const multer = require("multer");
 const cors = require("cors");
+const path = require("path");
 
 
 app.use(express.json());
@@ -22,6 +23,26 @@ mongoose.connect(process.env.MONGODB_URI)
 app.get("/", (req, res) => {
     res.send("express app is running");
 });
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    },
+})
+
+const upload = multer({storage:storage})
+
+//creating upload endpoint for images
+
+app.use('/images', express.static('upload/images'))
+
+app.post("/upload",upload.single("product"),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${PORT}/images/${req.file.filename}`
+    })
+})
 
 // server
 app.listen(PORT, () => {
