39118070444982373bd072ad1aa4bd5ab5546e03
A SHIVA KUMAR <shivuas605@gmail.com>
2026-04-08 19:40:12 +0530

    product details into database

--- backend/index.js
+++ backend/index.js
@@ -81,6 +81,27 @@ const Product = mongoose.model("Product",{
     },
 })
 
+app.post('/addproducts', async (req,res) => {
+     const product = new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+     });
+     console.log(product);
+     await product.save();
+
+     console.log("saved");
+
+     res.json({
+        success:1,
+        name: req.body.name,
+     })
+     
+})
+
 
 // server
 app.listen(PORT, () => {
