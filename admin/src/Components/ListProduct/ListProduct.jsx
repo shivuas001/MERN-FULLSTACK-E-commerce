9be381502f75b453b6ef06aa81d5c492a08086f8
A SHIVA KUMAR <shivuas605@gmail.com>
2026-04-19 11:43:14 +0530

    ListProduct for adminpanel

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ListProduct.css'
-
+import cross_icon from '../../assets/cross_icon.png'
 const ListProduct = () => {
+
   return (
     <div className='list-product'>
-
+         <h1>All Product List</h1>
+         <div className="listproduct-format-main">
+            <p>Product</p>
+            <p>Title</p>
+            <p>Old Price</p>
+            <p>New Price</p>
+            <p>Category</p>
+            <p>Remove</p>
+         </div>
+        <div className="listproduct-allproducts">
+          <hr />
+          {allProducts.map((product,index)=>{
+              return <> 
+              <div key={index} className="listproduct-format-main listproduct-format">
+                  <img src={product.image} alt="" className="listproduct-product-icon" />
+                  <p>{product.name}</p>
+                  <p>${product.old_price}</p>
+                  <p>${product.new_price}</p>
+                  <p>{product.category}</p>
+                  <img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon' src={cross_icon} alt="" />
+              </div>
+              <hr/>
+            </>
+          })}
+        </div>
     </div>
   )
 }
