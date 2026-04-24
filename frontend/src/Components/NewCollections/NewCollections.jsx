1d4d31edf2bc9ddf57b72ee36760de6e7ee50386
A SHIVA KUMAR <shivuas605@gmail.com>
2026-04-24 19:37:45 +0530

    connected newcollection api endpoint to newcollection.jsx frontend

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './NewCollections.css'
-import new_collections from '../Assets/new_collections'
 import Items from '../Items/Items'
 
 const NewCollections = () => {
+
+  const [new_collections, setNew_collections] = useState([]);
+
+  useEffect(()=>{
+     fetch('http://localhost:4000/newcollections')
+     .then((Response)=>Response.json()).then((data)=>setNew_collections(data))
+  },[]);
   return (
     <div className='new-collections'>
         <h1>NEW COLLECTIONS</h1>
