ff48bbd4abae07214da5a74c3e6800425d49f139
A SHIVA KUMAR <shivuas605@gmail.com>
2026-04-25 21:27:20 +0530

    connected popularinwomen Api endpoint to frontend popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Popular.css'
-import data_product from '../Assets/data'
 import Items from '../Items/Items'
 
 const Popular = () => {
+
+  const [popularProducts, setPopularProducts] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/popularinwomen')
+    .then((Response)=>Response.json())
+    .then((data)=>setPopularProducts(data))
+  },[])
   return (
     <div className='popular'>
          <h1>POPULAR IN WOMENS</h1>
          <hr />
          <div className="popular-item">
-            {data_product.map((item, i)=>{
+            {popularProducts.map((item, i)=>{
                 return <Items key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}  />
             })}
          </div>
